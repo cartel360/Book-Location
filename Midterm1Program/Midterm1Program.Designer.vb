f5a304f04de5f2a03ea919cfe378d9773b46753a
Billy Okeyo <34124094+cartel360@users.noreply.github.com>
2020-10-02 00:20:00 +0300

    Add files via upload

---
+++ Midterm1Program/Midterm1Program.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Midterm1Program
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnLocation = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtLocation = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(597, 88)
+        Me.txtNumber.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(148, 26)
+        Me.txtNumber.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(438, 94)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Call Number"
+        '
+        'btnLocation
+        '
+        Me.btnLocation.Location = New System.Drawing.Point(551, 154)
+        Me.btnLocation.Name = "btnLocation"
+        Me.btnLocation.Size = New System.Drawing.Size(113, 35)
+        Me.btnLocation.TabIndex = 2
+        Me.btnLocation.Text = "Find Book"
+        Me.btnLocation.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(438, 233)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Location"
+        '
+        'txtLocation
+        '
+        Me.txtLocation.Location = New System.Drawing.Point(563, 227)
+        Me.txtLocation.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtLocation.Name = "txtLocation"
+        Me.txtLocation.Size = New System.Drawing.Size(182, 26)
+        Me.txtLocation.TabIndex = 4
+        '
+        'Midterm1Program
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1362, 741)
+        Me.Controls.Add(Me.txtLocation)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnLocation)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Midterm1Program"
+        Me.Text = "Midterm1 Program"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtNumber As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnLocation As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtLocation As System.Windows.Forms.TextBox
+
+End Class
---
+++ Midterm1Program/Midterm1Program.vb
@@ -0,0 +1,32 @@
+﻿Public Class Midterm1Program
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnLocation.Click
+        Dim Mynumber As Integer
+        Mynumber = Val(txtNumber.Text)
+        If Mynumber = 100 And Mynumber <= 199 Then
+            txtLocation.Text = "Basement"
+        Else
+            If Mynumber >= 200 And Mynumber <= 500 Or Mynumber > 900 Then
+                txtLocation.Text = "Main Floor"
+            Else
+                If Mynumber >= 501 And Mynumber <= 900 And Not Mynumber >= 700 And Mynumber <= 750 Then
+                    txtLocation.Text = "Upper Floor"
+                Else
+                    If Mynumber >= 700 And Mynumber <= 750 Then
+                        txtLocation.Text = "Archives"
+
+                    End If
+                End If
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Midterm1Program_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
